3044e9428e54a320a75e6c941524ed98dd951fb6
Lucifer Avada <lucifer.avada@gmail.com>
2019-01-30 14:02:13 -0500

    use VueRouter

--- application/src/app.js
+++ application/src/app.js
@@ -1,9 +1,29 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import VueRouter from 'vue-router';
+
+import App from './App.vue';
+import Test from './components/Test.vue';
 
 import '../assets/app.sass';
 
-new Vue({
-  el: '#app',
-  render: h => h(App)
-})
+
+async function main() {
+
+  Vue.use(VueRouter);
+
+  const routes = [
+    { path: '/foo', component: Test }
+  ];
+
+  const router = new VueRouter({
+    routes
+  });
+
+  const app = new Vue({
+    router,
+    render: h => h(App)
+  }).$mount('#app');
+
+}
+
+main();
