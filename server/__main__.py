a6b31dc0da24f6dd73bf0d93d22092bf4586b411
Lucifer Avada <lucifer.avada@gmail.com>
2019-06-11 22:34:20 -0400

    import handlers from __main__.py

--- server/__main__.py
+++ server/__main__.py
@@ -1,4 +1,14 @@
-import handlers
+from os import listdir
+from os.path import dirname, join
+from importlib import import_module
+
+files = filter(lambda file: file.endswith('.py'), \
+    listdir(join(dirname(__file__), 'handlers')))
+
+for file in files:
+    module = file.split('.')[0]
+    import_module(f'.{module}', package=f'handlers')
+
 import resource
 import seed
 
