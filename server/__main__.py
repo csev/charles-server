5bd3541f73aec19ce9aa4f9060f93cea9d7ae408
Paul Severance <paul.severance@gmail.com>
2020-01-26 10:44:44 -0500

    remove websocket option

--- server/__main__.py
+++ server/__main__.py
@@ -1,8 +1,5 @@
 import argparse
 
-from sanic.log import logger
-from sanic.websocket import WebSocketProtocol
-
 import handlers
 import resource
 import seed
@@ -13,17 +10,12 @@ from server import server
 
 parser = argparse.ArgumentParser(description='Fire Server')
 
-parser.add_argument('--websocket', action='store_const', const=True)
 parser.add_argument('--workers', default=1)
 parser.add_argument('--host', default='0.0.0.0')
 parser.add_argument('--port', default=8001)
+parser.add_argument('--debug', action='store_const', const=True, default=False)
 
 args = parser.parse_args()
 
 
-if args.websocket:
-    logger.info('Starting Realtime Server...')
-    server.run(host=args.host, port=args.port, workers=args.workers, protocol=WebSocketProtocol)
-else:
-    logger.info('Starting API Server...')
-    server.run(host=args.host, port=args.port, workers=args.workers)
+server.run(host=args.host, port=args.port, debug=args.debug, workers=args.workers)
