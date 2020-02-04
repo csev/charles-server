302ca6a31d73dc3c0803b9e1efcbf97fdfdf1069
Paul Severance <paul.severance@gmail.com>
2020-02-04 15:25:14 -0500

    add requirements and elasticsearch resource

--- server/resource.py
+++ server/resource.py
@@ -1,3 +1,5 @@
+from fire_elasticsearch import Elasticsearch
+
 from server import server
 
 from authentication import Authentication
@@ -6,5 +8,7 @@ from models.user import User
 
 
 server.blueprint(Authentication.resource(url_prefix='/v1'))
+
+server.blueprint(Elasticsearch.resource(url_prefix='/v1'))
 
 server.blueprint(User.resource(url_prefix='/v1', realtime=True))
